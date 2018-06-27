8c5e0b4d03de29c9fb52a060a99bae67702dcaf8
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-27 17:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function findMatching(drivers, name){
 
 function fuzzyMatch(drivers, letters) {
   return drivers.filter(function(driver){
-    reutrn driver.startsWith(letters)
+    return driver.startsWith(letters)
   })
 }
