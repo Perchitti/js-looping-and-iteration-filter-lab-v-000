b95b8b64b36240da8221d2fd041e89349bdca2ef
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-27 17:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Code your solution in this file
-function findMatching(drivers){
-  for const (drivers of collection) {
-    if drivers === drivers.name
-    console.log(name)
+function findMatching(drivers, string){
+  for (const drivers of collection) {
+    if (drivers.name === string) {
+      console.log(drivers.name)
+    }
   }
 }
